12efe8f656764c73576c212b6742662b2f3233a4
giAptri <argyaarya14@gmail.com>
2023-02-21 12:18:33 +0700

    php pertemuan1

---
+++ p1-gya-ti05/index.php
@@ -0,0 +1,10 @@
+<?php
+// definisi variable (pakai dollar)
+    $nama = 'argya arya putri';
+    $umur = 18;
+    $berat = 45;
+
+    echo 'Nama Saya :' .$nama;
+    echo '<br>Umur Saya :' .$umur;
+    echo '<br>Berat Saya :'.$berat;
+?>
